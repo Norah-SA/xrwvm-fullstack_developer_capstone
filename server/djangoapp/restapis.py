a1a231676aa389a5a39e8e2a80cf21b45eebec80
Norah-SA <norah.alabbad0@gmail.com>
2024-07-20 17:47:12 +0300

    10.0

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,8 @@
 # Uncomment the imports below before you add the function code
 import requests
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv   # type: ignore
+from requests.exceptions import RequestException
 
 load_dotenv()
 
@@ -16,11 +17,10 @@ sentiment_analyzer_url = os.getenv(
 
 
 def get_request(endpoint, **kwargs):
-    
     params = ""
-    if(kwargs):
+    if (kwargs):
         for key, value in kwargs.items():
-            params = params + key+ "=" + value+ "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -29,9 +29,10 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except RequestException as e:
         # If any error occurs
         print("Network exception occurred")
+        print(f"Request failed: {e}")
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
@@ -53,8 +54,9 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except RequestException as e:
         print("Network exception occurred")
+        print(f"Request failed: {e}")
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .restapis import get_request, analyze_review_sentiments, post_review
+# from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -14,16 +14,19 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path(route='login', view=views.login_user, name='login'),
-    #path for logout
+    # path for logout
     path(route='logout',  view=views.logout_request, name="logout"),
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships, name='get_dealers_by_state'),
     # path for add a review view
 
-    #path get car 
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    # path get car
+    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
