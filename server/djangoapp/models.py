0b3f5232fbdac49f881d033a06872606da6f8430
Norah <88266321+Norah-SA@users.noreply.github.com>
2024-07-20 04:56:57 +0300

    9.5

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -33,7 +33,7 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake,
-                      on_delete=models.CASCADE)  # Many-to-One relationship
+               on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         # car body types
@@ -51,7 +51,7 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-           validators=[
+        validators=[
                         MaxValueValidator(2024),
                         MinValueValidator(2015)
            ])
